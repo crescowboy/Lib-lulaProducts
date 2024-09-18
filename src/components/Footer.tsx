2c4e35ee7a74ad9394639ba2ba8ac0a6deb19c77
wcrespo <wellingtoncrespo2001@gmail.com>
2024-09-18 12:25:36 -0500

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="bg-gray-800 p-4 text-white text-center">
-        &copy; 2024 Catálogo de Productos. Todos los derechos reservados.
+        &copy; 2024 Product Catalog. All rights reserved.
       </footer>
     );
   }
